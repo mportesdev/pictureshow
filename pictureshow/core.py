d7eff6220a4b8b5867a9d6951b2409e7ba713ac8
myrmica-habilis <michalportes1@gmail.com>
2020-11-22 16:48:07 +0100

    Implement saving mulitple pictures to one PDF

--- pictureshow/core.py
+++ pictureshow/core.py
@@ -4,32 +4,35 @@ from reportlab.pdfgen.canvas import Canvas
 
 
 class PictureShow:
-    def __init__(self, pic_file):
-        self.pic_file = pic_file
+    def __init__(self, *pic_files):
+        self.pic_files = pic_files
 
     def save_pdf(self, pdf_file, page_size=A4, margin=0, stretch_small=False):
         page_width, page_height = page_size[0], page_size[1]
         area_width, area_height = page_width - 2*margin, page_height - 2*margin
 
-        picture = ImageReader(self.pic_file)
-        pic_width, pic_height = picture.getSize()
-        pic_is_big = pic_width > area_width or pic_height > area_height
-        pic_is_wide = pic_width / pic_height > area_width / area_height
-
-        # calculate scale factor to fit picture to area
-        if pic_is_big or stretch_small:
-            scale = (area_width / pic_width if pic_is_wide
-                     else area_height / pic_height)
-            pic_width = pic_width * scale
-            pic_height = pic_height * scale
-
-        # center picture to area
-        x = margin + (area_width - pic_width) / 2
-        y = margin + (area_height - pic_height) / 2
-
         pdf_canvas = Canvas(pdf_file, pagesize=page_size)
-        pdf_canvas.drawImage(picture, x, y,
-                             width=pic_width, height=pic_height)
+        for pic_file in self.pic_files:
+            picture = ImageReader(pic_file)
+            pic_width, pic_height = picture.getSize()
+            pic_is_big = pic_width > area_width or pic_height > area_height
+            pic_is_wide = pic_width / pic_height > area_width / area_height
+
+            # calculate scale factor to fit picture to area
+            if pic_is_big or stretch_small:
+                scale = (area_width / pic_width if pic_is_wide
+                         else area_height / pic_height)
+                pic_width = pic_width * scale
+                pic_height = pic_height * scale
+
+            # center picture to area
+            x = margin + (area_width - pic_width) / 2
+            y = margin + (area_height - pic_height) / 2
+
+            pdf_canvas.drawImage(picture, x, y,
+                                 width=pic_width, height=pic_height)
+            pdf_canvas.showPage()
+
         pdf_canvas.save()
 
 
