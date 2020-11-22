7003a198ca18a1e27def71e0c76fa25b46fb4724
myrmica-habilis <michalportes1@gmail.com>
2020-11-22 22:59:18 +0100

    Update CLI to allow saving multiple pictures

--- pictureshow/cli.py
+++ pictureshow/cli.py
@@ -1,19 +1,19 @@
 import argparse
 import os
 
-from pictureshow.core import picture_to_pdf
+from pictureshow.core import pictures_to_pdf
 
 
 def main():
     parser = argparse.ArgumentParser('pictureshow')
-    parser.add_argument('-i', '--input', '--picture',
-                        dest='picture', required=True, help='input file path')
-    parser.add_argument('-o', '--output', '--pdf',
-                        dest='pdf', required=True, help='output file path')
+    parser.add_argument('-i', '--pictures', nargs='+', required=True,
+                        help='input picture file path(s)', dest='picture')
+    parser.add_argument('-o', '--pdf', required=True,
+                        help='output PDF file path', dest='pdf')
     args = parser.parse_args()
 
-    picture_path = os.path.abspath(args.picture)
+    picture_paths = [os.path.abspath(pic_file) for pic_file in args.picture]
     pdf_path = os.path.abspath(args.pdf)
 
-    picture_to_pdf(picture_path, pdf_path)
+    pictures_to_pdf(*picture_paths, pdf_file=pdf_path)
     print(f'Saved to file: {pdf_path}')
