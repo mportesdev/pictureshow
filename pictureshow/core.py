869f2a143e3021cba967b4997166511ceeeb2e95
myrmica-habilis <michalportes1@gmail.com>
2020-11-26 16:56:26 +0100

    Refactor code to method

--- pictureshow/core.py
+++ pictureshow/core.py
@@ -8,8 +8,7 @@ class PictureShow:
         self.pic_files = pic_files
 
     def save_pdf(self, pdf_file, page_size=A4, margin=0, stretch_small=False):
-        page_width, page_height = page_size[0], page_size[1]
-        area_width, area_height = page_width - 2*margin, page_height - 2*margin
+        area_size = page_size[0] - 2*margin, page_size[1] - 2*margin
 
         pdf_canvas = Canvas(pdf_file, pagesize=page_size)
         ok, errors = 0, 0
@@ -20,29 +19,40 @@ class PictureShow:
                 errors += 1
                 continue
 
-            pic_width, pic_height = picture.getSize()
-            pic_is_big = pic_width > area_width or pic_height > area_height
-            pic_is_wide = pic_width / pic_height > area_width / area_height
-
-            # calculate scale factor to fit picture to area
-            if pic_is_big or stretch_small:
-                scale = (area_width / pic_width if pic_is_wide
-                         else area_height / pic_height)
-                pic_width = pic_width * scale
-                pic_height = pic_height * scale
-
-            # center picture to area
-            x = margin + (area_width - pic_width) / 2
-            y = margin + (area_height - pic_height) / 2
-
-            pdf_canvas.drawImage(picture, x, y,
-                                 width=pic_width, height=pic_height)
+            x, y, pic_width, pic_height = self._position_and_size(
+                picture.getSize(), area_size, stretch_small
+            )
+            pdf_canvas.drawImage(
+                picture, margin + x, margin + y, pic_width, pic_height
+            )
             pdf_canvas.showPage()
             ok += 1
 
         if ok != 0:
             pdf_canvas.save()
         return ok, errors
+
+    @staticmethod
+    def _position_and_size(pic_size, area_size, stretch_small):
+        """Calculate position and size of the picture in the area."""
+        pic_width, pic_height = pic_size
+        area_width, area_height = area_size
+
+        pic_is_big = pic_width > area_width or pic_height > area_height
+        pic_is_wide = pic_width / pic_height > area_width / area_height
+
+        # calculate scale factor to fit picture to area
+        if pic_is_big or stretch_small:
+            scale = (area_width / pic_width if pic_is_wide
+                     else area_height / pic_height)
+            pic_width = pic_width * scale
+            pic_height = pic_height * scale
+
+        # center picture to area
+        x = (area_width - pic_width) / 2
+        y = (area_height - pic_height) / 2
+
+        return x, y, pic_width, pic_height
 
 
 def pictures_to_pdf(*pic_files, pdf_file=None, page_size=A4, margin=0,
