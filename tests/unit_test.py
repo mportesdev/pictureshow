6f43b4b5b8fec4e43cbb96206c1455d3e287b6e6
myrmica-habilis <michalportes1@gmail.com>
2021-01-13 17:46:54 +0100

    Remove unused code, move constants

--- tests/unit_test.py
+++ tests/unit_test.py
@@ -4,8 +4,11 @@ import pytest
 
 from pictureshow.core import PictureShow
 from pictureshow.exceptions import PageSizeError, MarginError, LayoutError
-from tests import A4_WIDTH, A4_LENGTH, A4, A4_LANDSCAPE
 
+A4_WIDTH = 72 * 210 / 25.4
+A4_LENGTH = 72 * 297 / 25.4
+A4 = A4_WIDTH, A4_LENGTH
+A4_LANDSCAPE = A4_LENGTH, A4_WIDTH
 A4_PORTRAIT_MARGIN_72 = (A4_WIDTH - 144, A4_LENGTH - 144)
 A4_LANDSCAPE_MARGIN_72 = (A4_LENGTH - 144, A4_WIDTH - 144)
 
@@ -60,7 +63,6 @@ class TestSavePdf:
             mock_canvas.save.assert_not_called()
 
 
-@pytest.mark.current
 class TestValidatePageSize:
     """Test core.PictureShow._validate_page_size"""
 
