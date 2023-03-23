9d797fea8a6eef3f2a6a5e36e0ac8b489a5936e7
Michal Porteš <michalportes1@gmail.com>
2023-03-23 13:31:34 +0100

    Bump version to 0.8.2

--- pictureshow/__init__.py
+++ pictureshow/__init__.py
@@ -1,7 +1,7 @@
 from .core import PictureShow, pictures_to_pdf
 from .exceptions import PageSizeError, MarginError, LayoutError
 
-__version__ = '0.8.1'
+__version__ = '0.8.2'
 
 __all__ = ['__version__', 'PictureShow', 'pictures_to_pdf',
            'PageSizeError', 'MarginError', 'LayoutError']
