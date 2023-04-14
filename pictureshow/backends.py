6171a74fb1d9b13a84c1bfd6b4d9e6ee034a03ed
Michal Porteš <michalportes1@gmail.com>
2023-04-14 10:57:59 +0200

    Move more logic to ReportlabBackend
    - add `read_errors` class attribute
    - add `read_picture` method
    - add `get_picture_size` method

--- pictureshow/backends.py
+++ pictureshow/backends.py
@@ -1,12 +1,25 @@
+from PIL import UnidentifiedImageError
+from reportlab.lib.utils import ImageReader
 from reportlab.pdfgen.canvas import Canvas
 
 
 class ReportlabBackend:
+    read_errors = (
+        OSError,  # file does not exist or is a dir
+        UnidentifiedImageError,  # file not recognized as picture
+    )
+
     def init(self, output_file, page_size):
         self.canvas = Canvas(output_file, pagesize=page_size)
 
     def add_page(self):
         self.canvas.showPage()
+
+    def read_picture(self, picture):
+        return ImageReader(picture)
+
+    def get_picture_size(self, picture):
+        return picture.getSize()
 
     def add_picture(self, picture, x, y, width, height):
         self.canvas.drawImage(picture, x, y, width, height, mask='auto')
